d128561fdc9c3aa742395724d51f8e1e0186d891
joric <joric@users.noreply.github.com>
2020-06-05 23:27:47 +0500

    updated files

--- skins/update_game.cmd
+++ skins/update_game.cmd
@@ -1,6 +1,5 @@
 @echo off
-
-set image=images\v_portalgun.joric.vtf
+set image=images\v_portalgun.stamp.vtf
 
 set portal2=C:\Program Files (x86)\Steam\steamapps\common\Portal 2
 set path=%portal2%\bin;%path%
@@ -9,13 +8,7 @@ set pakdir=pak01_dir\materials\models\weapons\v_models\v_portalgun
 
 md %pakdir%
 
-copy %image% %pakdir% >nul
-
-vpk pak01_dir > nul
-
-echo Copying pak01_dir.vpk to %portal2%\portal2_dlc3.
-
-copy pak01_dir.vpk "%portal2%\portal2_dlc3"  >nul
+copy %image% %pakdir% && vpk pak01_dir > nul && copy pak01_dir.vpk "%portal2%\portal2_dlc3"
 
 rd /S /Q pak01_dir
 del pak01_dir.vpk
