1ca6cbd03e8e561dba4d9574fc64cbc4b3fbcee1
joric <joric@users.noreply.github.com>
2020-06-10 10:35:36 +0500

    updated files

--- skins/update_game.cmd
+++ skins/update_game.cmd
@@ -1,12 +1,7 @@
 @echo off
 setlocal EnableDelayedExpansion
 
-set image=images\v_portalgun.joric.vtf
-
-if "%1" neq "" (
-set image=images\out.vtf
-python png2vtf.py images\v_portalgun.joric.png images\out.vtf %1
-)
+python png2vtf.py images\v_portalgun.joric.png images\v_portalgun.vtf %1
 
 set portal2=C:\Program Files (x86)\Steam\steamapps\common\Portal 2
 set path=%portal2%\bin;%path%
@@ -15,7 +10,7 @@ set pakdir=pak01_dir\materials\models\weapons\v_models\v_portalgun
 
 md %pakdir%
 
-copy %image% %pakdir%\v_portalgun.vtf && vpk pak01_dir > nul && copy pak01_dir.vpk "%portal2%\portal2_dlc3"
+copy images\v_portalgun.vtf %pakdir% && vpk pak01_dir > nul && copy pak01_dir.vpk "%portal2%\portal2_dlc3"
 
 rd /S /Q pak01_dir
 del pak01_dir.vpk
