e7240ec616f9251eb49f6d8bdd04dc5e4311c119
joric <joric@users.noreply.github.com>
2020-06-10 10:29:32 +0500

    added png2vtf.py

---
+++ skins/png2vtf.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+from PIL import Image, ImageDraw, ImageFont
+from sys import argv
+
+def convert(infile, outfile, text):
+    im = Image.open(infile)
+    draw = ImageDraw.Draw(im)
+    font = ImageFont.truetype(r'C:\Windows\Fonts\arial.ttf', 50) 
+    draw.text((380, 20), text, fill='black', font = font)
+    im.putalpha(255)
+    data = im.tobytes('raw')
+    f = open(outfile,'wb')
+    f.write(bytes.fromhex('56 54 46 00 07 00 00 00 02 00 00 00 50 00 00 00 00 04 00 04 00 23 00 00 01 00 00 00 00 00 00 00 90 68 B0 3E 19 87 B9 3E 36 21 BC 3E 00 00 00 00 00 00 80 3F 0C 00 00 00 01 FF FF FF FF 00 00 01 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00'))
+    f.write(data)
+
+if __name__=='__main__':
+    if len(argv)<3:
+        print('usage: png2vtf.py infile outfile text')
+    else:
+        infile = argv[1]
+        outfile = argv[2]
+        text = argv[3] if len(argv)>3 else ''
+        print('converting ', infile, outfile, text)
+        convert(infile, outfile, text)
+
--- skins/update_game.cmd
+++ skins/update_game.cmd
@@ -1,5 +1,12 @@
 @echo off
+setlocal EnableDelayedExpansion
+
 set image=images\v_portalgun.joric.vtf
+
+if "%1" neq "" (
+set image=images\out.vtf
+python png2vtf.py images\v_portalgun.joric.png images\out.vtf %1
+)
 
 set portal2=C:\Program Files (x86)\Steam\steamapps\common\Portal 2
 set path=%portal2%\bin;%path%
@@ -8,7 +15,7 @@ set pakdir=pak01_dir\materials\models\weapons\v_models\v_portalgun
 
 md %pakdir%
 
-copy %image% %pakdir% && vpk pak01_dir > nul && copy pak01_dir.vpk "%portal2%\portal2_dlc3"
+copy %image% %pakdir%\v_portalgun.vtf && vpk pak01_dir > nul && copy pak01_dir.vpk "%portal2%\portal2_dlc3"
 
 rd /S /Q pak01_dir
 del pak01_dir.vpk
