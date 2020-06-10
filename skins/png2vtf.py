cea99cc87c2f110185e72b4cfabb06daa1326a2c
joric <joric@users.noreply.github.com>
2020-06-11 00:38:10 +0500

    went RGB888 (no alpha)

--- skins/png2vtf.py
+++ skins/png2vtf.py
@@ -6,12 +6,11 @@ from sys import argv
 def convert(infile, outfile, text):
     im = Image.open(infile)
     draw = ImageDraw.Draw(im)
-    font = ImageFont.truetype(r'C:\Windows\Fonts\arial.ttf', 40) 
+    font = ImageFont.truetype(r'C:\Windows\Fonts\arial.ttf', 30)
     draw.text((380, 20), text, fill='grey', font = font)
-    im.putalpha(255)
     data = im.tobytes('raw')
     f = open(outfile,'wb')
-    f.write(bytes.fromhex('56 54 46 00 07 00 00 00 02 00 00 00 50 00 00 00 00 04 00 04 00 23 00 00 01 00 00 00 00 00 00 00 90 68 B0 3E 19 87 B9 3E 36 21 BC 3E 00 00 00 00 00 00 80 3F 0C 00 00 00 01 FF FF FF FF 00 00 01 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00'))
+    f.write(bytes.fromhex('56 54 46 00 07 00 00 00 02 00 00 00 50 00 00 00 00 04 00 04 00 03 00 00 01 00 00 00 00 00 00 00 1A 68 B0 3E EE 05 B9 3E A3 75 BC 3E 00 00 00 00 00 00 80 3F 03 00 00 00 01 FF FF FF FF 00 00 01 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00'))
     f.write(data)
 
 if __name__=='__main__':
