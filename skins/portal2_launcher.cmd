5c543cd70c2bc00a58bf056d013dbf1d346301ad
joric <joric@users.noreply.github.com>
2020-06-14 13:03:01 +0500

    updated files

--- skins/portal2_launcher.cmd
+++ skins/portal2_launcher.cmd
@@ -1,3 +1,4 @@
 cd %~dp0
 call update_game.cmd
+timeout 2
 start steam://rungameid/620
--- skins/update_game.cmd
+++ skins/update_game.cmd
@@ -7,6 +7,7 @@ FOR /F "tokens=* USEBACKQ" %%F IN (`bash -c ./fetch.sh`) DO SET text=%%F
 echo %text% maps played so far
 
 python png2vtf.py images\v_portalgun.png images\v_portalgun.vtf %text%
+::python png2vtf.py images\v_portalgun.stamp.png images\v_portalgun.vtf
 
 set pakdir=pak01_dir\materials\models\weapons\v_models\v_portalgun
 md %pakdir%
